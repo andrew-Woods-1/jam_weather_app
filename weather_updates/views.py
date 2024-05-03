c549864827072eace259ed3dbc347c2d6385e4de
Masonac <masonac00@gmail.com>
2024-05-03 18:35:02 -0500

    updating code to Jacobs version

--- weather_updates/views.py
+++ weather_updates/views.py
@@ -16,11 +16,21 @@ def index(request):
             API_KEY = '7fbda08677dac82a98a9081c2ae362bc'
             # getting the city name from the form input   
             city_name = request.POST.get('city')
-            state_code = request.POST.get('state_code')
+            state = request.POST.get('state_code')
+            country = request.POST.get('country')
             # the url for current weather, takes city_name and API_KEY   
-            url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{state_code}&appid={API_KEY}&units=imperial'
+            url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name},{state},{country}&appid={API_KEY}&units=imperial'
             # converting the request response to json   
             response = requests.get(url).json()
+
+            lat = response['coord']['lat']
+            lon = response['coord']['lon']
+            limit = 2
+
+            url_state = f'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API_KEY}'
+            response_state = requests.get(url_state).json()
+
+
             # getting the current time
             current_time = datetime.now()
             # formatting the time using directives, it will take this format Day, Month Date Year, Current Time 
@@ -28,11 +38,15 @@ def index(request):
             # bundling the weather information in one dictionary
             city_weather_update = {
                 'city': city_name,
+                'state': response_state[0]['state'],
                 'description': response['weather'][0]['description'],
                 'icon': response['weather'][0]['icon'],
                 'temperature': 'Temperature: ' + str(response['main']['temp']) + ' °F',
+                'real_feel': 'Feels Like: ' +str(response['main']['feels_like']) + ' °F',
+                'daily_max' : 'Daily High: ' + str(response['main']['temp_max']) + ' °F',
+                'daily_min' : 'Daily Low: ' + str(response['main']['temp_min']) + ' °F',
                 'country_code': response['sys']['country'],
-                'wind': 'Wind: ' + str(response['wind']['speed']) + 'mph',
+                'wind': 'Wind: ' + str(response['wind']['speed']) + ' mph',
                 'humidity': 'Humidity: ' + str(response['main']['humidity']) + '%',
                 'time': formatted_time
             }
