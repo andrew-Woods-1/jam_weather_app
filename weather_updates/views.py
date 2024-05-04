487408b7f66ef14d9e2e1af29d78768e3cb7ef27
Jacob <harves02@gmail.com>
2024-05-03 21:16:03 -0500

    final alterations v4

--- weather_updates/views.py
+++ weather_updates/views.py
@@ -7,34 +7,42 @@ from datetime import datetime
 
 # Create your views here.
 
-# the index() will handle all the app's logic
+# index view to handle app logic
 def index(request):
-    # if there are no errors the code inside try will execute
+    # try block if POST
     try:
     # checking if the method is POST
         if request.method == 'POST':
+
+            # Store the API key in a constant variable
             API_KEY = '7fbda08677dac82a98a9081c2ae362bc'
+
             # getting the city name from the form input   
             city_name = request.POST.get('city')
 
-            # the url for current weather, takes city_name and API_KEY   
+            # the url for current weather, takes city_name and API_KEY variables 
             url = f'https://api.openweathermap.org/data/2.5/weather?q={city_name}&appid={API_KEY}&units=imperial'
-            # converting the request response to json   
+
+            # get the response
             response = requests.get(url).json()
 
+            # Use the response to get the entered city's latitude and longitude
             lat = response['coord']['lat']
             lon = response['coord']['lon']
             limit = 2
 
+            # Access the GeoCoding API to get the value of the state the city exists in, if any
             url_state = f'http://api.openweathermap.org/geo/1.0/reverse?lat={lat}&lon={lon}&limit={limit}&appid={API_KEY}'
+
+            # Create new response object for this API call
             response_state = requests.get(url_state).json()
 
-
-            # getting the current time
+            # get the current time
             current_time = datetime.now()
-            # formatting the time using directives, it will take this format Day, Month Date Year, Current Time 
+            # format the time
             formatted_time = current_time.strftime("%A, %B %d %Y, %H:%M:%S %p")
-            # bundling the weather information in one dictionary
+
+            # create the dictionary with values supplied from API
             city_weather_update = {
                 'city': city_name + ",",
                 'state': response_state[0]['state'],
@@ -53,8 +61,10 @@ def index(request):
         else:
             city_weather_update = {}
         context = {'city_weather_update': city_weather_update}
+
+        # render the page with the supplied data
         return render(request, 'weather_updates/home.html', context)
-    # if there is an error the 404 page will be rendered 
-    # the except will catch all the errors 
+    
+    # Catch errors and render the 404.html page
     except:
         return render(request, 'weather_updates/404.html')
